63b78528ee66e14a0c20073068b30c94d512ef5e
Batuhan Gonenc <gonencbthn@gmail.com>
2020-08-09 15:28:02 +0300

    g-postman-main

--- g-postman.py
+++ g-postman.py
@@ -36,8 +36,7 @@ now_hour =now.strftime("%H")
 now_minute=now.strftime("%M")
 #---------------------------
 
-print("""*\nG-Postman Copyright (C) 2020  Batuhan Gonenc\n*
-~
+print("""~
 G-Postman
 ~
 Postman's calendar --> {}
@@ -51,6 +50,7 @@ Enjoy.
 
 #Options
 ---------
+0-Gmail to Protonmail
 1-Gmail to Gmail
 2-Gmail to Yahoo
 3-Gmail to Yaani
@@ -69,11 +69,68 @@ mx.music.play(999)
 #where-all-the-things-go-on
 while True:
     s1 = input("**********\nchoose one(for options 'o'.):")
+    
     if s1 =="q":
         print("Good Bye.")
         sys.exit()
+        
     elif s1 =='o':
         print("----------\n1-gmail to gmail\n2-gmail to special mail\n3-special mail to gmail\n4-special mail to special mail\nfor exit 'q'.\n----------")
+        
+    elif s1 == "0":
+        sendermail = input("*****\nsender e-mail(gmail):")
+
+        sendermail += "@gmail.com"
+
+        senderpassword=input("*****\npassword:")
+        subject = input("subject:")
+
+        receiver = input("*****\nreceiver e-mail(gmail):")
+        receiver += "@gmail.com"
+
+        switcher1 = input("*****\nWhich one do you prefer?\nto leave a little message by hand(m)\nenter the path that shows the document.(p)")
+
+        if switcher1 == "m":
+            content = input("*****\nenter your message:")
+
+        elif switcher1 == "p":
+            pathto = input("*****\nenter the path(it must be in .txt format.):")
+            try:
+                file = open(pathto,"r")
+                content = file.read()
+                file.close()
+            except:
+                print("----------\n#something went wrong#\n#try to check out the file#\n#it can be missed or moved.#")
+                continue
+        else:
+            print("#unvalid process code#")
+            break
+
+        message = MIMEMultipart("alternative")
+
+        message["Subject"] = subject
+        message["From"] = sendermail
+        message["To"] = receiver
+
+        message_content = MIMEText(content,"plain")
+        message.attach(message_content)
+
+        context = ssl.create_default_context()
+        try:
+            with smtplib.SMTP_SSL("smtp.gmail.com",465,context = context)as server:
+                server.login(sendermail,senderpassword)
+                server.sendmail(
+                sendermail,receiver,message.as_string()
+                )
+                file = open("mailimg.txt","r")
+                img = file.read()
+                file.close()
+                print("-----><-----\n{}-----><-----".format(img))
+                print("*****************\nemail sent successfull.\nHave a nice day!")
+        except:
+            print("****************\nsomething went wrong.")
+            
+            
     elif s1 == "1":
         sendermail = input("*****\nsender e-mail(gmail):")
 
@@ -126,6 +183,8 @@ while True:
                 print("*****************\nemail sent successfull.\nHave a nice day!")
         except:
             print("****************\nsomething went wrong.")
+    
+    
     elif s1 =="2":
         sendermail = input("*****\nsender e-mail(gmail):")
 
@@ -178,6 +237,8 @@ while True:
                 print("*****************\nemail sent successfull.\nHave a nice day!")
         except:
             print("****************\nsomething went wrong.")
+    
+    
     elif s1 =="3":
         sendermail = input("*****\nsender e-mail(gmail):")
         sendermail +="@gmail.com"
@@ -229,6 +290,8 @@ while True:
                 print("*****************\nemail sent successfull.\nHave a nice day!")
         except:
             print("****************\nsomething went wrong.")
+    
+    
     elif s1 =="4":
         sendermail = input("*****\nsender e-mail(gmail):")
         sendermail += "@gmail.com"
@@ -280,7 +343,10 @@ while True:
                 print("*****************\nemail sent successfull.\nHave a nice day!")
         except:
             print("****************\nsomething went wrong.")
+    
+    
     elif s1 =="5":
+    
         sendermail = input("*****\nsender e-mail(gmail):")
 
         sendermail += "@gmail.com"
