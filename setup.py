baf9e16d1b15c7373fb27a053a4f6bdef187794b
Batuhan Gonenc <gonencbthn@gmail.com>
2020-08-09 15:29:57 +0300

    setup

--- setup.py
+++ setup.py
@@ -37,13 +37,13 @@ except:
     sys.exit()
 
 here = os.getcwd()
-new = '\nalias postman="cd {} && python3 postman.py"'.format(here)
+new = '\nalias gpostman="cd {} && python3 postman.py"'.format(here)
 newcontent = content + new
 
 file = open("/home/{}/.bashrc".format(un),'w')
 file.write(newcontent)
 file.close()
 
-print("G-Postman installed successfully.\njust write postman in terminal to use.")
+print("G-Postman installed successfully.\njust write gpostman in terminal to use.")
 li = input('press enter to exit')
 os.remove(setup.py)
